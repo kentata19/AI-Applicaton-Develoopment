4b0575ea6b49f1bf890123617b1d4ce7bb7b138b
kentata19 <ke.asada@raksul.com>
2023-08-06 11:34:23 +0900

    summarize long youtube video

--- app/summarize-youtube-video.py
+++ app/summarize-youtube-video.py
@@ -1,10 +1,12 @@
 import streamlit as st
+from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
 
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 def init_page():
@@ -18,13 +20,18 @@ def init_page():
 
 
 def select_model():
-    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-3.5-16k", "GPT-4"))
     if model == "GPT-3.5":
-        model_name = "gpt-3.5-turbo"
+        st.session_state.model_name = "gpt-3.5-turbo-0613"
+    elif model == "GPT-3.5":
+        st.session_state.model_name = "gpt-3.5-turbo-16k-0613"
     else:
-        model_name = "gpt-4"
+        st.session_state.model_name = "gpt-4"
 
-    return ChatOpenAI(temperature=0, model_name=model_name)
+    # 300: 本文以外の指示のtoken数 (以下同じ)
+    # 要約指示を書くためのtoken数の分を除外しておかないとエラーが出るので注意
+    st.session_state.max_token = OpenAI.modelname_to_contextsize(st.session_state.model_name) - 300
+    return ChatOpenAI(temperature=0, model_name=st.session_state.model_name)
 
 
 def get_url_input():
@@ -32,7 +39,6 @@ def get_url_input():
     return url
 
 
-# 事前に `pip install youtube-transcript-api pytube` の実行が必要
 def get_document(url):
     with st.spinner("Fetching Content ..."):
         loader = YoutubeLoader.from_youtube_url(
@@ -40,31 +46,42 @@ def get_document(url):
             add_video_info=True,  # タイトルや再生数も取得できる
             language=['en', 'ja']  # 英語→日本語の優先順位で字幕を取得
         )
-        return loader.load()
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name=st.session_state.model_name,
+            chunk_size=st.session_state.max_token,
+            chunk_overlap=0,
+        )
+        return loader.load_and_split(text_splitter=text_splitter)
 
 
 def summarize(llm, docs):
     prompt_template = """Write a concise Japanese summary of the following transcript of Youtube Video.
 
-============
-
 {text}
 
-============
-
-ここから日本語で書いてね
-必ず3段落以内の200文字以内で簡潔にまとめること:
+ここから日本語で書いてね:
 """
     PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
 
     with get_openai_callback() as cb:
         chain = load_summarize_chain(
             llm,
-            chain_type="stuff",
+            chain_type="map_reduce",
             verbose=True,
-            prompt=PROMPT
+            # 分割されたDocumentを要約する際のプロンプト
+            map_prompt=PROMPT,
+            # 個別の要約をくっつけた中間生成物を要約する際のプロダクト
+            combine_prompt=PROMPT
         )
-        response = chain({"input_documents": docs}, return_only_outputs=True)
+        response = chain(
+            {
+                "input_documents": docs,
+                # token_max を指示しないと、GPT3.5など通常の
+                # モデルサイズに合わせた内部処理になってしまうので注意
+                "token_max": st.session_state.max_token
+            },
+            return_only_outputs=True
+        )
 
     return response['output_text'], cb.total_cost
 
@@ -78,8 +95,8 @@ def main():
 
     with container:
         url = get_url_input()
-        if url:
-            document = get_document(url)
+        document = get_document(url)
+        if document:
             with st.spinner("ChatGPT is typing ..."):
                 output_text, cost = summarize(llm, document)
             st.session_state.costs.append(cost)
